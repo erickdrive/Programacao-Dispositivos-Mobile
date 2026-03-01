b26702c1a2f54d081decc5637e3cc7c5c530211c
ERICK OLIVEIRA DE SOUZA <erickoliveiradesouza2021@gmail.com>
2026-02-28 21:32:23 -0300

    Implementação das Funcionalidades

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,111 @@
-import { SetStateAction, useState } from "react";
+import { SetStateAction, use, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 
 export default function RootLayout() {
-  const [numero, setNumero] = useState(0);
+  const [numero1, setNumero1] = useState('');
+  const [numero2, setNumero2] = useState('');
+  const [display, setDisplay] = useState('0');
+  const [alternarNumero, setAlternarNumero] = useState(false);
+  const [fimConta, setFimConta] = useState(false);
   const [operador, setOperador] = useState('');
-  const [display, setDisplay] = useState('');
+  const [resultado, setResultado] = useState(0);
+
+
+  //split
+  // 1 variavel primeiro valor 
+  // 2 variavel operador 
+  // 3 variavel Segundo valor
   
   function limparTela(){
-    setNumero(0);
+    setNumero1('');
+    setNumero2('');
     setOperador('');
-    setDisplay('');
+    setDisplay('0');
+    setAlternarNumero(false);
+    setResultado(0);
   }
-   
   
-  
-  function calcular(){
-    
+  function definirOperador(op: string){
+    setOperador(op);
+    setAlternarNumero(true);
+    setDisplay(display + " " + op + " ");
   }
 
+  function calcular(){
+    let valor = 0;
+
+    switch(operador){
+      case '+': 
+        valor = Number(numero1) + Number(numero2);
+      break;
+      case '-': 
+        valor = Number(numero1) - Number(numero2);
+      break;
+      case '/': 
+        if(Number(numero2) === 0){
+          setDisplay('Erro não é possivel realizar divisão por 0');
+          setFimConta(true);
+          return;
+        }else{
+          valor = Number(numero1) / Number(numero2);
+        }
+      break;
+      case '*': 
+        valor = Number(numero1) * Number(numero2);
+      break;
+      case 'V': 
+        if(Number(numero1) < 0){
+          setDisplay('Erro o número é negativo');
+          setFimConta(true);
+          return;
+        }else{
+          valor = Math.sqrt(Number(numero1));
+        }
+      break;
+      case '%': 
+        if(numero2 !== ''){
+          valor = (Number(numero1) * Number(numero2)) / 100; // se tiver numero2, calcula a porcentagem relativo ao numero1
+        }else{
+          valor = Number(numero1) / 100; //transforma numero1 em fração
+        }
+      break;
+    }
+
+    setResultado(valor);
+    setDisplay(String(valor));
+    setNumero1(String(valor));
+    setNumero2('');
+    setOperador('');
+    setAlternarNumero(false);
+    setFimConta(true);
+  }
+
+  function concatenarValores(numero:string){
+    if (fimConta) {
+      limparTela();
+      setDisplay(numero);
+      setNumero1(numero);
+      setFimConta(false);
+      return;
+    }
+
+    if(alternarNumero == false){
+      let primeiroValor = numero1 + numero;
+      setNumero1(primeiroValor);
+
+      if(display === '0'){
+        setDisplay(numero);
+      }else{
+        setDisplay(display + numero);
+      }
+    }else{
+      let segundoValor = numero2 + numero;
+      setNumero2(segundoValor)
+      setDisplay(display + numero);
+    }
+  }
+   
   return (
     <>
       <View>
@@ -26,61 +113,61 @@ export default function RootLayout() {
             <Text style={styles.textoDisplay}>CALCULADORA</Text>
           </View>
           <View style={styles.display}>
-            <Text style={styles.textoDisplay}>0</Text>
+            <Text style={styles.textoDisplay}>{display}</Text>
           </View>
           <View style={styles.teclado}>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> limparTela()}>
               <Text style={styles.textoBotaoDigitos}>C</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> definirOperador('V')}>
               <Text style={styles.textoBotaoDigitos}>V</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> definirOperador('%')}>
               <Text style={styles.textoBotaoDigitos}>%</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> definirOperador('/')}>
               <Text style={styles.textoBotaoDigitos}>/</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('7')}>
               <Text style={styles.textoBotaoDigitos}>7</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('8')}>
               <Text style={styles.textoBotaoDigitos}>8</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('9')}>
               <Text style={styles.textoBotaoDigitos}>9</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> definirOperador('*')}>
               <Text style={styles.textoBotaoDigitos}>*</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('4')}>
               <Text style={styles.textoBotaoDigitos}>4</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('5')}>
               <Text style={styles.textoBotaoDigitos}>5</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('6')}>
               <Text style={styles.textoBotaoDigitos}>6</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> definirOperador('-')}>
               <Text style={styles.textoBotaoDigitos}>-</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('1')}>
               <Text style={styles.textoBotaoDigitos}>1</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('2')}>
               <Text style={styles.textoBotaoDigitos}>2</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('3')}>
               <Text style={styles.textoBotaoDigitos}>3</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> definirOperador('+')}>
               <Text style={styles.textoBotaoDigitos}>+</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={()=> concatenarValores('0')}>
               <Text style={styles.textoBotaoDigitos}>0</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.botaoCalcular}>
+            <TouchableOpacity style={styles.botaoCalcular} onPress={()=> calcular()}>
               <Text style={styles.textoBotao}>CALCULAR</Text>
             </TouchableOpacity>
           </View>
